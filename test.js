cfd43424fa5d519eb14c975a6b746840aa1c52f1
Jessan23 <jessicaanairayagrez@gmail.com>
2024-10-27 22:19:26 +0000

    3ra entrega, faltaba tobe

--- test.js
+++ test.js
@@ -10,17 +10,18 @@ test("One euro should be 1.07 dollars", function() {
 
     
     const dollars = fromEuroToDollar(3.5);
-
-
+    expect(dollars).toBe(3.745); 
 })
+
+
 
 test("One dollar should be 156.5 yens", function() {
     
     const { fromDollarToYen } = require('./app.js');
 
 
-    const Yen = fromDollarToYen(156.5);
-
+    const Yen = fromDollarToYen(1);
+    expect(Yen).toBe(156.5);
 
 })
 test("One yen should be 0.87 pound", function() {
@@ -28,7 +29,8 @@ test("One yen should be 0.87 pound", function() {
     const { fromYenToPound } = require('./app.js');
 
     
-    const Pound = fromYenToPound(0.87);
+    const Pound = fromYenToPound(1);
+    expect(Pound).toBe(0.87); 
+});
 
 
-})
